4faa346a1be6be2d90feb2706924833f5b067ad8
Julia Davydova <juliada888@gmail.com>
2026-05-13 12:21:11 +0300

    add third exersice

--- main.py
+++ main.py
@@ -83,3 +83,37 @@ print(merg_values)
 
 print(f"min value: {merg_values[0]}")
 print(f"max value: {merg_values[-1]}")
+
+# EXERCISE 3: Working with List of Dictionaries
+
+# Using a list of 2 dictionaries:
+
+employees = [{
+  "name": "Tina",
+  "age": 30,
+  "birthday": "1990-03-10",
+  "job": "DevOps Engineer",
+  "address": {
+    "city": "New York",
+    "country": "USA"
+  }
+},
+{
+  "name": "Tim",
+  "age": 35,
+  "birthday": "1985-02-21",
+  "job": "Developer",
+  "address": {
+    "city": "Sydney",
+    "country": "Australia"
+  }
+}]
+
+# Prints out - the name, job and city of each employee using a loop. The program must work for any number of employees in the list, not just 2.
+
+for employee in employees:
+    print(f"name: {employee['name']}, job: {employee['job']}, city: {employee["address"]["city"]}")
+
+# Prints the country of the second employee in the list by accessing it directly without the loop.
+
+print (employees[1]["address"]["country"])
