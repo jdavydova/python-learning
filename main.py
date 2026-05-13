ef7504810b4f900c4f468151ab122b8218a7e6e2
Julia Davydova <juliada888@gmail.com>
2026-05-13 11:55:41 +0300

    add first exersice

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-import requests
+# EXERCISE 1: Working with Lists
+my_list = [1, 2, 2, 4, 4, 5, 6, 8, 10, 13, 22, 35, 52, 83]
 
-response = requests.get("https://gitlab.com/api/v4/users/juliada888/projects")
-print(response.json())
-print(type(response.json()))
+# Write a program that prints out all the elements of the list that are higher than or equal to 10.
+for item in my_list:
+    if item >= 10:
+        print(item)
 
-my_project = response.json()
+# Instead of printing the elements one by one, make a new list that has all the elements higher than or equal to 10 from this list in it and print out this new list.
+new_list = []
 
-for project in my_project:
-    print(f"Project Name {project['name']}\nProject URL {project['web_url']}")
+for item in my_list:
+    if item >= 10:
+        new_list.append(item)
+
+print(new_list)
+
+# Ask the user for a number as input and print a list that contains only those elements from my_list that are higher than the number given by the user.
+
+user_number = int(input("Enter a number: "))
+new_list = []
+
+for item in my_list:
+    if item > user_number:
+        new_list.append(item)
+
+print(new_list)
+
